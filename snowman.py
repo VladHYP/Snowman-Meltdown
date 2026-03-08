09bbc9035e2fe82fc5c0f5e66f31b80ecc7cf2cf
Vladimir Leutar <vladimir.leutar@gmail.com>
2026-03-08 15:17:30 +0000

    Add hidden word display with underscores

--- snowman.py
+++ snowman.py
@@ -3,4 +3,6 @@ import random
 words = ["meltdown", "snowman", "winter", "frozen", "blizzard"]
 secret_word = random.choice(words)
 
-print(secret_word)
+display_word = ["_"] * len(secret_word)
+
+print("Word:", " ".join(display_word))
